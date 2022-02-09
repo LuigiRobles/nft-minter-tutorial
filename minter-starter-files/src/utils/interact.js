cc29a5fe8c93b90c3fcc12dd2dd44efaace9ee14
Luigi.Robles <Luigi.robles0846@my.riohondo.edu>
2022-02-08 22:16:46 -0800

    update.1 mintSite tutorial

---
+++ minter-starter-files/src/utils/interact.js
@@ -0,0 +1,33 @@
+export const connectWallet = async () => {
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_requsetAccounts",
+            });
+            const obj = {
+                status: "👆🏽 Write a message in the text-field above.",
+                address: addressArray[0],
+            };
+            return obj;
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        } else {
+            return {
+                address: "",
+                status: (
+                    <span> 
+                        <p>
+                          {" "}
+                          🦊{" "}
+                           <a target="_blank"  href={`https://metamask.io/download.html`}>
+                    </p>
+                    </span>
+                )
+            }
+
+        }
+    }
+}
